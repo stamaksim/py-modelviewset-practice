f7c50d3c1dfb43c8956a3574ae515110475030d8
Maksim Stahovskij <maksss196@gmail.com>
2024-04-24 21:24:34 +0300

    py-modelviewset-practice

--- author/urls.py
+++ author/urls.py
@@ -1,3 +1,14 @@
-# Create your urls here
+from django.urls import path, include
+from author.views import AuthorViewSet
+from rest_framework import routers
 
-urlpatterns = []
+app_name = "author"
+
+router = routers.DefaultRouter()
+
+router.register("", AuthorViewSet, basename="manage")
+
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
